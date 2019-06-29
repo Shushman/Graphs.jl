47dc41c00e470b4e77c7f985a7024ebe7f5e2d75
Shushman <shushmanchoudhury@gmail.com>
2019-06-29 13:22:16 +0530

    Renamed and edited a_star_implicit

--- src/a_star_implicit_light.jl
+++ src/a_star_implicit_light.jl
@@ -1,8 +1,20 @@
-## A-Star shortest path algorithm with two specific assumptions
-## 1. Graph is implicit, i.e. edges are generated just-in-time by a neighbor function
-## 2. The graph is just a vector of indices, i.e. actual metadata stored outside
+# A* shortest-path search with two generalizations over a_star_spath:
+# 1. The graph is implicit; edges are not explicitly stored but instead generated
+# during vertex expansion by a user-defined visitor function
+# 2. The include_vertex! visitor function can also ADD new vertices, i.e. the size
+# of the graph can increase during the search
+#
+# We do not bother with deleting vertices as it is typically not worth the effort
+# for implicit search
+
+
 """
-Data structure for heap entry associated with each expanded vertex
+Data structure for heap entry associated with each expanded vertex.
+
+Attributes:
+    - `vIdx::Int` The integer index of the vertex being expanded
+    - `gvalue::D` The cost-to-come of the vertex from the source
+    - `fvalue::D` The cost-to-come + heuristic cost-to-go to the goal
 """
 struct AStarHEntry{D <: Number}
     vIdx::Int
@@ -14,7 +26,8 @@ Base.isless(e1::AStarHEntry, e2::AStarHEntry) = (e1.fvalue < e2.fvalue) || (e1.f
 
 
 """
-These are all dictionaries now because they depend on the number of vertices, which is variable
+Compared with a_star_spath, the attributes of AStarStates are all Dicts instead of
+fixed-length Vectors, as arbitrarily many vertices can be added by the visitors.
 """
 mutable struct AStarStates{D<:Number}
     parent_indices::Dict{Int,Int}
@@ -25,7 +38,7 @@ mutable struct AStarStates{D<:Number}
 end
 
 
-function create_astar_states(g::AbstractGraph{V}, ::Type{D}) where {V, D <: Number}
+function create_a_star_states(g::AbstractGraph{V}, ::Type{D}) where {V, D <: Number}
     parent_indices = Dict{Int,Int}()
     dists = Dict{Int,D}()
     colormap = Dict{Int,Int}()
@@ -41,8 +54,8 @@ function set_source!(state::AStarStates{D}, s::Int) where {D <: Number, V}
 end
 
 """
-Execute expand operation on the chosen node, while implicitly generating its neighbors based on a 
-visitor method, and populating `neighbors` with the neighboring vertices
+Execute expand operation on the chosen node, while implicitly generating its neighbors based on a
+visitor method, and populating `neighbors` with the neighboring vertices.
 """
 function process_neighbors_implicit!(
     state::AStarStates{D},
@@ -55,21 +68,26 @@ function process_neighbors_implicit!(
     dv = zero(D)
 
     for iv in neighbors
+
         # Default color 0
         v_color = get(state.colormap, iv, 0)
 
         if v_color == 0
+
             # Inserting for the first time
-            # @show u,iv
             state.dists[iv] = dv = du + edge_wt_fn(graph.vertices[u], graph.vertices[iv])
             state.parent_indices[iv] = u
             state.colormap[iv] = 1
             Graphs.discover_vertex!(visitor, graph.vertices[u], graph.vertices[iv], dv)
-
             state.hmap[iv] = push!(state.heap, AStarHEntry(iv, dv, dv + heuristic(graph.vertices[iv])))
+
         elseif v_color == 1
+
             dv = du + edge_wt_fn(graph.vertices[u], graph.vertices[iv])
+
+            # Update cost-to-come if cheaper from current parent
             if dv < state.dists[iv]
+
                 state.dists[iv] = dv
                 state.parent_indices[iv] = u
 
@@ -81,10 +99,10 @@ function process_neighbors_implicit!(
 end
 
 
-function astar_light_shortest_path_implicit!(
+function a_star_light_shortest_path_implicit!(
     graph::AbstractGraph{V},                # the graph
     edge_wt_fn::Function, # distances associated with edges
-    source::Int,             # the source
+    source::Int,             # the source index
     visitor::AbstractDijkstraVisitor,# visitor object
     heuristic::Function,      # Heuristic function for vertices
     state::AStarStates{D}) where {V, D <: Number}
@@ -92,16 +110,19 @@ function astar_light_shortest_path_implicit!(
     d0 = zero(D)
     set_source!(state, source)
 
+    # Will be populated by include_vertex!
     source_nbrs = Vector{Int}(undef,0)
 
-    if !Graphs.include_vertex!(visitor, graph.vertices[source], graph.vertices[source], d0, source_nbrs)
+    # Call the user-defined visitor function for expanding the source
+
+    if Graphs.include_vertex!(visitor, graph.vertices[source], graph.vertices[source], d0, source_nbrs) == false
         return state
     end
 
     process_neighbors_implicit!(state, graph, edge_wt_fn, source_nbrs, source, d0, visitor, heuristic)
     Graphs.close_vertex!(visitor, graph.vertices[source])
 
-    while !isempty(state.heap)
+    while ~(isempty(state.heap))
 
         # pick next vertex to include
         entry = pop!(state.heap)
@@ -110,9 +131,10 @@ function astar_light_shortest_path_implicit!(
 
         state.colormap[ui] = 2
 
-        nbrs = Vector{Int}(undef,0)
+        # Will be populated by include_vertex!
+        nbrs = Vector{Int}(undef, 0)
 
-        if !Graphs.include_vertex!(visitor, graph.vertices[state.parent_indices[ui]], graph.vertices[ui], du, nbrs)
+        if Graphs.include_vertex!(visitor, graph.vertices[state.parent_indices[ui]], graph.vertices[ui], du, nbrs) == false
             return state
         end
 
@@ -124,13 +146,36 @@ function astar_light_shortest_path_implicit!(
     state
 end
 
-function astar_light_shortest_path_implicit(
+function a_star_light_shortest_path_implicit!(
     graph::AbstractGraph{V},                # the graph
     edge_wt_fn::Function, # distances associated with edges
     source::Int,
     visitor::AbstractDijkstraVisitor,
     heuristic::Function = n -> 0,
     ::Type{D} = Float64) where {V, D <: Number}
-    state = create_astar_states(graph, D)
-    astar_light_shortest_path_implicit!(graph, edge_wt_fn, source, visitor, heuristic, state)
+    state = create_a_star_states(graph, D)
+    a_star_light_shortest_path_implicit!(graph, edge_wt_fn, source, visitor, heuristic, state)
+end
+
+"""
+Given the AStarStates result, extract the shortest path
+"""
+function shortest_path_indices(state::AStarStates{D}, graph::AbstractGraph{V},
+                       source::V, target::V) where {D, V}
+
+    source_idx = vertex_index(graph, source)
+    target_idx = vertex_index(graph, target)
+
+    @assert haskey(state.parent_indices, target_idx) == true "Target has no parent!"
+
+    sp = [target_idx]
+
+    # Walk back from target to source using parent indices
+    curr_idx = target_idx
+    while curr_idx != source_idx
+        curr_idx = state.parent_indices[curr_idx]
+        pushfirst!(sp, curr_idx)
+    end
+
+    return sp
 end
