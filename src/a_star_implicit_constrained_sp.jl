d562ebe43a2ec4ad9891f2e0e11808ffacdcc964
Shushman <shushmanchoudhury@gmail.com>
2019-08-11 20:43:59 -0700

    First pass of constrained astar done

--- src/a_star_implicit_constrained_sp.jl
+++ src/a_star_implicit_constrained_sp.jl
@@ -3,33 +3,33 @@
 ## constraints that depend on other weight functions
 struct AStarMCSPHEntry{D <: Number}
     v_idx::Int64
+    parent_idx::Int64
     gvalue::D
     fvalue::D
     weights::Vector{D}
-    self_handle::Int # refers to
     parent_handle::Int64    # refers to the heap handle in CLOSED LIST that tracks this path
 end
 
 Base.isless(e1::AStarMCSPHEntry, e2::AStarMCSPHEntry) = (e1.fvalue, e1.gvalue) < (e2.fvalue, e2.gvalue) # Lexicographic
 
 @with_kw mutable struct AStarMCSPStates{D <: Number}
-    open_list::BinaryMinHeap{AStarMCSPHEntry{D}}    = BinaryMinHeap{AStarMCSPHEntry{D}}()
-    closed_list::BinaryMinHeap{AStarMCSPHEntry{D}}  = BinaryMinHeap{AStarMCSPHEntry{D}}()
-    open_list_hmap::Dict{Int64,Set{Int64}}          = Dict{Int64,Set{Int64}}()
-    closed_list_hmap::Dict{Int64,Set{Int64}}        = Dict{Int64,Set{Int64}}()
+    open_list::MutableBinaryMinHeap{AStarMCSPHEntry{D}}    = MutableBinaryMinHeap{AStarMCSPHEntry{D}}()
+    closed_list::MutableBinaryMinHeap{AStarMCSPHEntry{D}}  = MutableBinaryMinHeap{AStarMCSPHEntry{D}}()
+    open_list_hmap::Dict{Int64,Set{Int64}}                 = Dict{Int64,Set{Int64}}()
+    closed_list_hmap::Dict{Int64,Set{Int64}}               = Dict{Int64,Set{Int64}}()
 end
 
 function is_dominated(state::AStarMCSPStates{D}, s::AStarMCSPHEntry{D}) where {D <: Number}
 
     for ol_idx in state.open_list_hmap[s.v_idx]
-        ols_entry = state.open_list[ol_idx]
+        ols_entry = state.open_list.nodes[state.open_list.node_map[ol_idx]]
         if isless(ols_entry, s)
             return true
         end
     end
 
     for cl_idx in state.closed_list_hmap[s.v_idx]
-        cls_entry = state.closed_list[cl_idx]
+        cls_entry = state.closed_list.nodes[state.closed_list.node_map[ol_idx]]
         if isless(cls_entry, s)
             return true
         end
@@ -38,24 +38,63 @@ function is_dominated(state::AStarMCSPStates{D}, s::AStarMCSPHEntry{D}) where {D
     return false
 end
 
+
+function is_eligible(entry::AStarMCSPHEntry{D},
+                     nbr::Int64,
+                     weight_functions::Vector{Function},
+                     weight_heuristics::Vector{Function},
+                     weight_constraints::Vector{D}) where {D <: Number}
+
+    n_weights = length(weight_functions)
+    new_weight_vector = zeros(D, n_weights)
+
+    for i = 1:n_weights
+        new_constr_wt = entry.weights[i] +  weight_functions[i](entry.v_idx, nbr) +
+                        weight_heuristics[i](nbr)
+        if new_constr_wt  > weight_constraints[i]
+            return false, new_weight_vector
+        else
+            new_weight_vector[i] = new_constr_wt
+        end
+    end
+
+    return true, new_weight_vector
+end
+
 function process_neighbors_implicit!(
     state::AStarMCSPStates{D},
     graph::AbstractGraph{V},
     edge_wt_fn::Function,
     neighbors::Vector{Int64},
     entry::AStarMCSPHEntry{D},
-    entry_handle::Int64,    # For closed list
+    entry_cl_handle::Int64,    # For closed list
     visitor::AbstractDijkstraVisitor,
     heuristic::Function,
+    weight_functions::Vector{Function},
     weight_heuristics::Vector{Function},
     weight_constraints::Vector{D}
     )
 
-    for iv in neighbors
+    for nbr_idx in neighbors
 
-        # Iterate over neighbors and add if eligible and not dominated
-        # Pay attention to parent handle etc.
+        is_elig, new_wt_vector =  is_eligible(entry, nbr_idx, weight_functions, weight_heuristics, weight_constraints)
 
+        if is_elig
+
+            # Create new entry
+            new_gvalue = entry.gvalue + edge_wt_fn(entry.v_idx, nbr_idx)
+            new_fvalue = new_gvalue + heuristic(nbr_idx)
+            new_entry = AStarMCSPHEntry(nbr_idx, entry.v_idx, new_gvalue, new_fvalue,
+                                        new_weight_vector, entry_cl_handle)
+
+            if ~(is_dominated(state, new_entry))
+                new_entry_handle = push!(state.open_list, new_entry)
+                push!(state.open_list_hmap, new_entry_handle)
+            end
+
+        end
+    end
+end
 
 
 
@@ -67,7 +106,9 @@ function a_star_constrained_shortest_path_implicit!(
     heuristic::Function,
     weight_heuristics::Vector{Function},
     weight_constraints::Vector{D},
-    state::AStarMCSPStates{D}) where {V, D <: Number}
+    ::Type{D} = Float64) where {V, D <: Number}
+
+    state = AStarMCSPStates{D}()
 
     n_weights = length(weight_constraints)
 
@@ -75,3 +116,52 @@ function a_star_constrained_shortest_path_implicit!(
     push!(state.open_list, source_entry)
 
     while ~(isempty(state.open_list))
+
+        # Remove from open_list and open_list_hmap
+        entry, entry_handle = top_with_handle(state.open_list)
+        pop!(state.open_list)
+        delete!(state.open_list_hmap[entry.v_idx], entry_handle)
+
+        nbrs = Vector{Int64}(undef, 0)
+
+        if Graphs.include_vertex!(visitor, graph.vertices[entry.parent_idx],
+                                  graph.vertices[entry.v_idx], entry.gvalue, nbrs) == false
+            return state, entry
+        end
+
+        # Insert into closed list to get new handle
+        cl_entry_handle = push!(state.closed_list, entry)
+        push!(state.closed_list_hmap[entry.v_idx], cl_entry_handle)
+
+        # Process nbrs
+        process_neighbors_implicit!(state, graph, edge_wt_fn, nbrs,
+                                    entry, cl_entry_handle, visitor,
+                                    heuristic, weight_functions, weight_heuristics, weight_constraints)
+        Graphs.close_vertex!(visitor, graph.vertices[entry.v_idx])
+    end
+
+    return state, source_entry
+end
+
+function shortest_path_cost_weights(state::AStarMCSPStates{D}, graph::AbstractGraph{V},
+                                    source::Int64, target_entry::AStarMCSPHEntry{D}) where {D <: Number}
+
+    target_idx = target_entry.v_idx
+
+    sp = [target_idx]
+
+    # Walk back USING closed list heap
+    curr_idx = target_idx
+    curr_par_handle = target_entry.parent_handle
+
+    while curr_idx != source
+
+        cl_list_entry = state.closed_list.nodes[state.closed_list.node_map[curr_par_handle]]
+
+        curr_idx = cl_list_entry.v_idx
+        curr_par_handle = cl_list_entry.parent_handle
+        pushfirst!(sp, curr_idx)
+    end
+
+    return sp, target_entry.gvalue, target_entry.weights
+end
